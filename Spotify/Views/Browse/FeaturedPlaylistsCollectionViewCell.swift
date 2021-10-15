9df34936a6cc2cdafa8ed7150bd8eacaa2df6136
Jihoi-Kang <jihoi.kang@gmail.com>
2021-10-15 15:50:43 +0900

    Add collection view on browse

---
+++ Spotify/Views/Browse/FeaturedPlaylistsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedPlaylistsCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Jihoi Kang on 2021/10/15.
+//
+
+import UIKit
+
+class FeaturedPlaylistsCollectionViewCell: UICollectionViewCell {
+    static let identifier = "FeaturedPlaylistsCollectionViewCell"
+}
---
+++ Spotify/Views/Browse/RecommendedTracksCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedTracksCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Jihoi Kang on 2021/10/15.
+//
+
+import UIKit
+
+class RecommendedTracksCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecommendedTracksCollectionViewCell"
+}
